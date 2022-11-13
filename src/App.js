f6cae96e67958f7cc21480ae4c771109a471620a
ju8ko <julia.x.kobzar@gmail.com>
2022-11-13 12:10:52 +0100

    Add cities API fetch.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import React, { useState } from "react";
 
+const options = {
+  method: "GET",
+  url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+  headers: {
+    "X-RapidAPI-Key": "bcebe823c6msh63f40eb68cc101fp11807djsn92379492b515",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+const fetchCities = (cityName) =>
+  fetch(
+    `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&namePrefix=${cityName}`,
+    options
+  );
+
 function App() {
-  const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
+  const [cityName, setCityName] = React.useState("");
+  const [matchedCities, setMatchedCities] = React.useState([]);
+
+  React.useEffect(() => {
+    if (cityName) {
+      console.log(cityName);
+      const getCities = async () => {
+        const response = await fetchCities(cityName);
+        const { data } = await response.json();
+        setMatchedCities(data?.filter((d) => d.type === "CITY"));
+      };
+
+      getCities();
+    }
+  }, [cityName]);
 
   return (
     <div className="App">
       <div className="search">
         <input
           type="text"
-          placeholder="Enter Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
+          placeholder="Enter city name"
+          value={cityName}
+          onChange={(e) => setCityName(e.target.value)}
           onKeyPress={() => {}}
         />
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p> {data.name}</p>
-            {data.sys ? <p>{data.sys.country}</p> : null}
-          </div>
-          <div className="temp">
-            {data.main ? <h1> {data.main.temp.toFixed()} F</h1> : null}
-          </div>
-          <div className="desc">
-            {data.weather ? <p>{data.weather[0].main} </p> : null}
-          </div>
-        </div>
-
-        <div className="bottom">
-          <div className="feels">
-            {data.main ? <p>{data.main.feels_like.toFixed()}F</p> : null}
-          </div>
-          <div className="humidity">
-            {data.main ? <p>{data.main.humidity} %</p> : null}
-          </div>
-          <div className="wind">
-            {data.wind ? <p>{data.wind.speed.toFixed()}MPH</p> : null}
-          </div>
-        </div>
+        <ul>
+          {matchedCities.map((city) => (
+            <li key={city.id}>{city.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
