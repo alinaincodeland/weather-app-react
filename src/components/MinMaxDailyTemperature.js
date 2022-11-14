e3ff96cb182b0fe6b53760e89ea6da06af958c96
ju8ko <julia.x.kobzar@gmail.com>
2022-11-14 15:39:18 +0100

    Add weather icons

--- src/components/MinMaxDailyTemperature.js
+++ src/components/MinMaxDailyTemperature.js
@@ -1,11 +1,17 @@
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+
 export function MinMaxDailyTemperature(props) {
-  const min = Math.min(...props.mins);
-  const max = Math.max(...props.maxs);
+  const min = Math.min(...props.mins).toFixed();
+  const max = Math.max(...props.maxs).toFixed();
 
   return (
     <ul>
-      <li>min temp: {min}℃</li>
-      <li>max temp: {max}℃</li>
+      <li>
+        <FaArrowDown className="arrow-down-icon" /> min {min}℃
+      </li>
+      <li>
+        <FaArrowUp className="arrow-up-icon" /> max {max}℃
+      </li>
     </ul>
   );
 }
